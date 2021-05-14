7ec594b7c6cf9797cbee7ee55bf676315a1894a8
Alan Zam <alnzam17@gmail.com>
2021-05-13 20:48:34 -0500

    feat(Samus): :poop: Add word collection saving (bad)
    This is an attempt to store messages into memory and send word collection to users privately

--- packages/samus/src/samus.js
+++ packages/samus/src/samus.js
@@ -1,15 +1,51 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-require('dotenv').config()
+require("dotenv").config();
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    console.log('Received a message!', msg)
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+const players = [];
+const wordCollection = [];
+
+client.on("message", (msg) => {
+  const player = { id: msg.author.id, username: msg.author.username };
+
+  if (!players.find((p) => p.id === player.id)) {
+    players.push(player);
+  }
+
+  wordCollection.push({
+    playerId: player.id,
+    word: msg.content,
+  });
+
+  players.forEach((p) => {
+    const wordCollectionAsList = wordCollection.reduce(
+      (acc, prev) => acc + prev,
+      ""
+    );
+    // console.log("Sending to player", p.id);
+    // console.log(wordCollectionAsList);
+  });
+
+  const replyFunc = async () => {
+    const user = await client.users.fetch("140585543229505536");
+
+    if (!user) return msg.channel.send("User not found :(");
+
+    await user.send("message").catch(() => {
+      msg.channel.send(
+        "User has DMs closed or has no mutual servers with the bot :("
+      );
+    });
+  };
+
+  //   replyFunc();
+
+  if (msg.content === "!ping") {
+    msg.reply("pong");
   }
 });
 
