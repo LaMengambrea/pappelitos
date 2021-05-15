0bd18c8924d333536473554b5d4e9b7519e257e3
Alan Zam <alnzam17@gmail.com>
2021-05-15 13:55:03 -0500

    feat(Samus): :sparkles: Include command handler

--- packages/samus/src/samus.js
+++ packages/samus/src/samus.js
@@ -2,6 +2,20 @@ require('dotenv').config();
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs
+  .readdirSync('./src/commands')
+  .filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`../src/commands/${file}`);
+  // set a new item in the Collection
+  // with the key as the command name and the value as the exported module
+  client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -12,6 +26,22 @@ const wordCollection = [];
 
 client.on('message', (msg) => {
   if (msg.author.bot) return;
+
+  let prefix = '!';
+  const args = msg.content.slice(prefix.length).trim().split(/ +/);
+
+  const commandName = args.shift().toLowerCase();
+
+  if (!client.commands.has(commandName)) return;
+
+  const command = client.commands.get(commandName);
+
+  try {
+    command.execute(msg, args);
+  } catch (error) {
+    console.error(error);
+    msg.reply('there was an error trying to execute that command!');
+  }
 
   const player = { id: msg.author.id, username: msg.author.username };
 
