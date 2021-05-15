8d12e6010ba7afd69eb85a0d8f3fc5c1c9478227
Alan Zam <alnzam17@gmail.com>
2021-05-15 13:48:58 -0500

    fix(Samus): :bug: Fix the basic word storage feature

--- packages/samus/src/samus.js
+++ packages/samus/src/samus.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
-require("dotenv").config();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -10,10 +10,13 @@ const players = [];
 const wordCollection = [];
 
 client.on("message", (msg) => {
+  if (msg.author.bot) return;
+
   const player = { id: msg.author.id, username: msg.author.username };
 
   if (!players.find((p) => p.id === player.id)) {
     players.push(player);
+    console.log("Players", players);
   }
 
   wordCollection.push({
@@ -21,32 +24,11 @@ client.on("message", (msg) => {
     word: msg.content,
   });
 
-  players.forEach((p) => {
-    const wordCollectionAsList = wordCollection.reduce(
-      (acc, prev) => acc + prev,
-      ""
-    );
-    // console.log("Sending to player", p.id);
-    // console.log(wordCollectionAsList);
-  });
-
-  const replyFunc = async () => {
-    const user = await client.users.fetch("140585543229505536");
-
-    if (!user) return msg.channel.send("User not found :(");
-
-    await user.send("message").catch(() => {
-      msg.channel.send(
-        "User has DMs closed or has no mutual servers with the bot :("
-      );
-    });
-  };
-
-  //   replyFunc();
-
-  if (msg.content === "!ping") {
-    msg.reply("pong");
-  }
+  const wordCollectionAsList = wordCollection.reduce(
+    (prev, acc) => prev + acc.word + "\n",
+    ""
+  );
+  msg.channel.send("Words:\n" + wordCollectionAsList);
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
