1cda16ab5a6c2434fa8076c48d45f9a9d9a1d859
Alan Zam <alnzam17@gmail.com>
2021-05-17 20:12:02 -0500

    style: :wrench: Fix formatting to pass prettier action

--- packages/samus/src/samus.js
+++ packages/samus/src/samus.js
@@ -1,22 +1,23 @@
-require("dotenv").config();
-const Discord = require("discord.js");
+require('dotenv').config();
+
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on("ready", () => {
+client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 const players = [];
 const wordCollection = [];
 
-client.on("message", (msg) => {
+client.on('message', (msg) => {
   if (msg.author.bot) return;
 
   const player = { id: msg.author.id, username: msg.author.username };
 
   if (!players.find((p) => p.id === player.id)) {
     players.push(player);
-    console.log("Players", players);
+    console.log('Players', players);
   }
 
   wordCollection.push({
@@ -25,10 +26,10 @@ client.on("message", (msg) => {
   });
 
   const wordCollectionAsList = wordCollection.reduce(
-    (prev, acc) => prev + acc.word + "\n",
-    ""
+    (prev, acc) => prev + acc.word + '\n',
+    ''
   );
-  msg.channel.send("Words:\n" + wordCollectionAsList);
+  msg.channel.send('Words:\n' + wordCollectionAsList);
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
